d00bd1ea21ef490d58cf3f0644aef98a4be8f0fb
chronvas <chron.vas@outlook.com>
2020-06-19 11:49:19 +0100

    tupples

--- some_vars/src/main.rs
+++ some_vars/src/main.rs
@@ -1,6 +1,22 @@
 fn main() {
    
     ints();
+    tupples_breakdown();
+}
+
+fn tupples_breakdown(){
+    let tup: (i64, i32, u8) = (400,200,3);
+    let big_tupple: (i64, i32, u8,u8,u8) = (400,200,3,2,1);
+    // breakdown
+    let (x,y,z) = tup;
+    println!("x = {}", x);
+    println!("y = {}", y);
+    println!("z = {}", z);
+
+    let a = tup.0;
+    println!("a = {}", a);
+
+    println!("big_tupple.4 = {}", big_tupple.4);
 }
 
 fn ints(){
@@ -12,7 +28,7 @@ fn ints(){
     x2 = 55;
     println!("X2 chanaged to {}", x2);
 
-    
+
     let x: i64 = 3;
     println!("x={} 😋😋😋",x);
 }
