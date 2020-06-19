f4d04b2767f65b9003134c62a67a2bd92cce6ae0
chronvas <chron.vas@outlook.com>
2020-06-19 11:39:51 +0100

    some ints

--- some_vars/src/main.rs
+++ some_vars/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 4;
+    println!("X is {}", x);
+
+    let mut x2 = 4;
+    println!("X2 is {}", x2);
+    x2 = 55;
+    println!("X2 chanaged to {}", x2);
+
+    ints();
+}
+
+fn ints(){
+    let x: i64 = 3;
+    println!("x={}",x);
 }
