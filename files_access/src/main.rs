4b0223fa8b093c56bc0b15b646e5e71cfd96e5a3
chronvas <chron.vas@outlook.com>
2020-07-03 12:09:58 +0100

    Read a file

---
+++ files_access/src/main.rs
@@ -0,0 +1,20 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn main() {
+    let sample_file = File::open("src/hello.txt");
+
+    let file_contents = match sample_file {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("Can't access file");
+        },
+    };
+
+    let mut buf_reader = BufReader::new(file_contents);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents);
+
+    println!("{}", contents);
+}
